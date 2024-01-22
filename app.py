adc26d6eb2677efe14b00fd5bbdef4a5463ca395
laldonate96 <lucasaldonate@gmail.com>
2024-01-22 01:02:57 -0300

    Pre Vercel deploy

--- app.py
+++ app.py
@@ -4,25 +4,22 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-def create_app():
-    app = Flask(__name__)
-    client = MongoClient(os.getenv("MONGODB_URI"))
-    app.db = client.microblog
+app = Flask(__name__)
+client = MongoClient(os.getenv("MONGODB_URI"))
+app.db = client.microblog
 
-    entries = []
+entries = []
 
-    @app.route('/', methods=["GET", "POST"])
-    def home():
-        if request.method == "POST":
-            entry_content = request.form.get("content")
-            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            entries.append((entry_content, formatted_date))
-            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
-            
-        entries_with_date = [
-            (entry['content'], entry['date'], datetime.datetime.strptime(entry['date'], "%Y-%m-%d").strftime("%b %d"))
-            for entry in app.db.entries.find({})
-        ]
-        return render_template('home.html', entries=entries_with_date)
-    
-    return app
+@app.route('/', methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((entry_content, formatted_date))
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+        
+    entries_with_date = [
+        (entry['content'], entry['date'], datetime.datetime.strptime(entry['date'], "%Y-%m-%d").strftime("%b %d"))
+        for entry in app.db.entries.find({})
+    ]
+    return render_template('home.html', entries=entries_with_date)
